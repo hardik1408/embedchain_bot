5188742365942584eb3a82212cefc71d7bb52a14
hardik1408 <hardikj22@iitk.ac.in>
2024-04-12 03:11:33 +0530

    rag-fusion

--- app.py
+++ app.py
@@ -5,17 +5,30 @@ from dotenv import load_dotenv
 load_dotenv() 
 
 app = App.from_config(config_path="config.yaml")
+app1 = App.from_config(config_path="config.yaml") 
+
+def generate_queries(message):
+    query="""You are a helpful assistant that generates multiple search queries based on a single input query.
+            Generate multiple search queries related to: {message}
+            OUTOUT 4 queries. Do not consider the input query as one of the output queries.
+            Do not consider any history of queries."""
+    
+    result=app1.query(query)
+    return result
 
 def chat(message, history):
     i = 0
     if i == 0:
         initial_msg = message
         app.add(initial_msg)
+        i=1
         yield "I have gathered necessary information , now you may ask questions."
-        i = 1
+        # i = 1
+    
+    # new_message = generate_queries(message)
     resp=app.query(message)
     answer_text = resp.split("Answer:")[1].strip()
-    return answer_text
+    yield answer_text
 
 gr.ChatInterface(
     chat,
