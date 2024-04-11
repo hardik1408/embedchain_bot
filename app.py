559a7925823651085c411f29f0ba9e81fec3ee9c
hardik1408 <hardikj22@iitk.ac.in>
2024-04-12 02:44:50 +0530

    code

--- app.py
+++ app.py
@@ -1,25 +1,24 @@
 import gradio as gr
 from embedchain import App
-import os
 from dotenv import load_dotenv
 
 load_dotenv() 
 
 app = App.from_config(config_path="config.yaml")
 
-def slow_echo(message, history):
+def chat(message, history):
     i = 0
     if i == 0:
         initial_msg = message
         app.add(initial_msg)
-        yield "I have gathered information about you, now you may ask questions."
+        yield "I have gathered necessary information , now you may ask questions."
         i = 1
     resp=app.query(message)
     answer_text = resp.split("Answer:")[1].strip()
-    yield answer_text
+    return answer_text
 
 gr.ChatInterface(
-    slow_echo,
+    chat,
     textbox=gr.Textbox(placeholder="Ask me a yes or no question", container=False, scale=7)
 ).launch()
 
